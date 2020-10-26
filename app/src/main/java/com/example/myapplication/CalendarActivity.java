532facbdcc6e767f908c91750215a81ef9c630f7
Daniel-Kong <danielkong03@gmail.com>
2020-10-25 21:44:25 -0700

    Calendar skeleton code

---
+++ app/src/main/java/com/example/myapplication/CalendarActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CalendarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        final CalendarView calendar = findViewById(R.id.calendarView);
+
+        Button eventButton = findViewById(R.id.button);
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                String selectedDate = sdf.format(new Date(calendar.getDate()));
+            }
+        });
+    }
+}
