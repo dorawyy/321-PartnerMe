b742fc092fdee1db0a3d304d9ed3898ef6189acc
Vincent <vincentyan8@hotmail.com>
2020-10-26 17:50:12 -0700

    added simple userObjectClass

---
+++ app/src/main/java/com/example/myapplication/UserObjectClass.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+import java.io.Serializable;
+
+public class UserObjectClass{
+    private String name;
+    private String _class;
+    private String language;
+    private String availability;
+    private String[] hobbies;
+    private String userId;
+
+}
