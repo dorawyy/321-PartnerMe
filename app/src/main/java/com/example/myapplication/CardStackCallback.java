2d3ccc8824d7c17b44895140d2931ad1be2f0646
Daniel-Kong <danielkong03@gmail.com>
2020-10-26 17:17:03 -0700

    Swipe activity. No users added yet

---
+++ app/src/main/java/com/example/myapplication/CardStackCallback.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+public class CardStackCallback extends DiffUtil.Callback {
+
+    private List<User> old, next;
+
+    public CardStackCallback(List<User> old, List<User> next) {
+        this.old = old;
+        this.next = next;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return old.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return next.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return old.get(oldItemPosition) == next.get(newItemPosition);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return old.get(oldItemPosition) == next.get(newItemPosition);
+    }
+}
