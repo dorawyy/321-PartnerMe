f4eb4e625ff762d1dde9dae0cd11c079c4052293
Vincent <vincentyan8@hotmail.com>
2020-11-03 00:07:30 -0800

    fixed fourth fragment to show up

--- app/src/main/java/com/example/myapplication/fourthFragment.java
+++ app/src/main/java/com/example/myapplication/fourthFragment.java
@@ -81,7 +81,7 @@ public class fourthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_fourth, container, false);
 
         final EditText nameField = view.findViewById(R.id.signup_nameField);
         final EditText languageField = view.findViewById(R.id.signup_Language);
